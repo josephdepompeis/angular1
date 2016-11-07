47772e99fb6f7559c8bdc8207343dcaac2984ff6
Joseph DePompeis <jdepompeis@revolutionehr.com>
2016-11-06 21:57:49 -0500

    some things

--- newNote/newNote.component.js
+++ newNote/newNote.component.js
@@ -4,6 +4,11 @@
     var newNoteModule = angular.module('newNoteModule', [])
       newNoteModule.component('newNote', {
         templateUrl: "newNote/newNote.component.html",
+        transclude: true,
+        bindings: {
+          match: "@",
+          playingAs: "@"
+        },
         controller: FormController,
         controllerAs: 'fc'
     });
@@ -12,36 +17,28 @@
 
       this.latestNote = newNoteService.latestNote;
 
-
-
       this.dashboardLink = function(){
         window.location = "#/dashboard";
       }
 
-
-
-
-
-
-        this.submit = function (form) {
-          console.log("yeahbitch")
+        this.submit = function (form ) {
+          console.log("yeahbitch");
+          console.log("this.match", this.match);
           if (form.$valid) {
-            console.log(form)
-            console.log(form.uPassword.$$lastCommittedViewValue);
+            console.log(form);
+            // console.log(form.uPassword.$$lastCommittedViewValue);
             console.log(form.uNote.$$lastCommittedViewValue);
             this.formResults = {
-                "match_id": form.uPassword.$$lastCommittedViewValue
+                "match_id": this.match
               , "name":  form.uNote.$$lastCommittedViewValue
             }
             console.log(this.formResults);
             newNoteService.submitNewNote( this.formResults ).then ( (data) => {
-            this.latestNote = newNoteService.latestNote;
-            });
-            this.latestNote = newNoteService.latestNote;
-            matchNotesService.getMatchNotes().then ( (data) => {
-              this.matchNotes = data;
-              console.log(this.matchNotes);
-
+              this.latestNote = newNoteService.latestNote;
+              matchNotesService.getMatchNotes().then ( (data) => {
+                this.matchNotes = data;
+                console.log("hello", this.matchNotes);
+              });
             });
           }
         };
